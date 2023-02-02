06f6982f5dbef69440df0708bb6d6acc0d989b92
chloeadriancreates <chloeadriancreates@gmail.com>
2023-02-02 09:19:20 +0100

    Fetch data from mock API

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,28 @@
 import "./App.scss";
 import Hero from "../Hero/Hero";
 import Header from "../Header/Header";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [lodgings, setLodgings] = useState();
+
+  const getLodgings = async() => {
+    try {
+      const response = await fetch("./lodgings.json");
+      setLodgings(await response.json());
+    } catch(error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getLodgings();
+  }, []);
+
+  useEffect(() => {
+    console.log(lodgings);
+  }, [lodgings]);
+
   return (
     <div className="app">
       <Header />
