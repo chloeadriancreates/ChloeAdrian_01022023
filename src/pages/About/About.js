a8e42cc165610db4c696394c7a073b2b72dd0175
chloeadriancreates <chloeadriancreates@gmail.com>
2023-02-04 11:30:24 +0100

    Edit Hero component to fit Home and About pages

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,10 +1,14 @@
-import Header from "../../components/Header/Header";
 import "./About.scss";
+import Header from "../../components/Header/Header";
+import Hero from "../../components/Hero/Hero";
+import Footer from "../../components/Footer/Footer";
 
 function About() {
     return (
         <div className="about">
             <Header />
+            <Hero />
+            <Footer />
         </div>
     );
 }
