d14cbdc515f7a8357752a832eaf1223ffd1fc92f
chloeadriancreates <chloeadriancreates@gmail.com>
2023-02-14 09:45:05 +0100

    Code cleanup

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,6 @@
 import "./Hero.scss";
 
 function Hero(props) {
-    // return (
-    //         <div className="hero">
-    //             <h2 className="hero-title">Chez vous,&nbsp;</h2>
-    //             <h2 className="hero-title">partout et ailleurs</h2>
-    //         </div>
-    // );
-
     return (props.home)
               ? <div className="hero hero--home">
                     <h2 className="hero-title">Chez vous,&nbsp;</h2>
--- src/components/LodgingCard/LodgingCard.js
+++ src/components/LodgingCard/LodgingCard.js
@@ -2,10 +2,12 @@ import "./LodgingCard.scss";
 import { Link } from "react-router-dom";
 
 function LodgingCard(props) {
-    const { id, title, cover } = props.data;
+    const { data } = props;
+    const { id, title, cover } = data;
 
     return (
-        <Link to={`/lodging/${id}`} state={{ data: props.data }} className="lodging" style={ {backgroundImage: `url(${cover})`} }>
+        <Link to={`/lodging/${id}`} state={{ data: data }}
+        className="lodging" style={ {backgroundImage: `url(${cover})`} }>
             <h3 className="lodging-title">{title}</h3>
         </Link>
     );
